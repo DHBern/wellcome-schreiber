c2388f708d570e939f57db92e15e096906e80e1a
Peter Daengeli <peter.daengeli@unibe.ch>
2024-11-05 23:45:50 +0100

    add Basel MS K II 11 (Hs. B)

---
+++ wellcome/BaselNI1_79/blattInfoDSZ.js
@@ -0,0 +1,17 @@
+/* Blattinformationen zu Doppelseiten
+ * Hier muss etwas gerechnet werden: Jede Zeile entspricht einer Doppelseite, d.h. bei Zeile
+ * <case "3": lagenNr=""; ...> befinden wir uns auf der dritten Doppelseite gerechnet von der ersten dargestellten Bild
+ * Beispiel:
+ * 		case "9": lagenNr="1/2"; lagenSymb="L5_L6"; lagenName="Quint./Sext."; konkordanz="12r–96v"; aktBuch = 1; imgDescr = false; break;
+ * resultiert in:
+ * 
+ * Lagenbeschreibung: "1/2. Lage, Quint./Sext."
+*/
+
+function blattInfoDS(blatt) {
+	switch(blatt) {
+		case "0": lagenNr=""; lagenSymb="blind"; lagenName=""; konkordanz=""; aktBuch = 0; imgDescr = false; break; 
+		case "1": lagenNr=""; lagenSymb="blind"; lagenName=""; konkordanz=""; aktBuch = 0; imgDescr = false; break;
+		return;
+	}
+}
